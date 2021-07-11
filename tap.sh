8419bdbb170275c1d94414a5aca69f0e3d0f9618
David Farrell <davidnmfarrell@gmail.com>
2021-07-11 10:07:40 -0400

    Simplify tap_ok, make end more useful
    Re-use pass/fail so there is only one path for emitting [not] ok.
    Makes tap_end accept an optional arg for emitting the test plan instead
    of the number of tests run.

--- tap.sh
+++ tap.sh
@@ -1,14 +1,5 @@
 TAP_TEST_COUNT=0
-TAP_TEST_FAIL_COUNT=0
-
-tap_ok() {
-  TAP_TEST_COUNT=$(( TAP_TEST_COUNT+1 ))
-  if [ "$1" != 1 ];then
-    TAP_TEST_FAIL_COUNT=$(( TAP_TEST_FAIL_COUNT+1 ))
-    printf "not "
-  fi
-  echo "ok $TAP_TEST_COUNT $2"
-}
+TAP_FAIL_COUNT=0
 
 tap_pass() {
   TAP_TEST_COUNT=$(( TAP_TEST_COUNT+1 ))
@@ -17,11 +8,21 @@ tap_pass() {
 
 tap_fail() {
   TAP_TEST_COUNT=$(( TAP_TEST_COUNT+1 ))
-  TAP_TEST_FAIL_COUNT=$(( TAP_TEST_FAIL_COUNT+1 ))
+  TAP__FAIL_COUNT=$(( TAP__FAIL_COUNT+1 ))
   echo "not ok $TAP_TEST_COUNT $1"
 }
 
 tap_end() {
-  echo "1..$TAP_TEST_COUNT"
-  exit "$TAP_TEST_FAIL_COUNT"
+  num_tests="$1"
+  [ -z "$num_tests" ] && num_tests="$TAP_TEST_COUNT"
+  echo "1..$num_tests"
+  exit $(( TAP_FAIL_COUNT > 0 ))
+}
+
+tap_ok() {
+  if [ "$1" -eq 1 ];then
+    tap_pass "$2"
+  else
+    tap_fail "$2"
+  fi
 }
--- tests/end.sh
+++ tests/end.sh
@@ -3,5 +3,10 @@
 
 out=$(TAP_TEST_COUNT=10;tap_end)
 [ "$out" != "1..10" ] && printf "not "
-echo "ok 1 end outputs the test plan (1..10==$out)"
-echo '1..1'
+echo "ok 1 end outputs the number of tests run (1..10==$out)"
+
+out=$(tap_end "5")
+[ "$out" != "1..5" ] && printf "not "
+echo "ok 2 end outputs the planned number of tests (1..5==$out)"
+
+echo '1..2'
